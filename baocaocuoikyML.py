51b6b774134d20f35e7cdb808a96196017d2992b
DuongVanLoi <duongloihk09@gmail.com>
2023-05-18 13:15:25 +0700

    tgm project

---
+++ baocaocuoikyML.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="BÀI BÁO CÁO CUỐI KỲ ",
+    
+    page_icon="👋",
+)
+st.write("# BÀI BÁO CÁO CUỐI KỲ  👋")
+
+st.sidebar.success("Select a demo above.")
+
+st.markdown(
+    """
+    
+    ### Môn học: Machine Laerning 
+    ### GVHD: TS Trần Tiến Đức
+    ### SVTH: 
+    Dương Văn Lợi - 20146366
+    
+    Nguyễn Cẩm Như - 20146065
+     
+
+        
+"""
+)
+
